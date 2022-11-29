2bf9d3bc5a0775de0d997786077e38478251c41f
Teagan glenn <teagan.m.glenn@gmail.com>
2022-11-28 18:26:40 -0700

    Abstract arg parsing

--- edgetpu_server/classify.py
+++ edgetpu_server/classify.py
@@ -29,7 +29,7 @@ import time
 
 import edgetpu_server.gstreamer as gstreamer
 from edgetpu_server.utils.svg import SVG
-from edgetpu_server.utils import avg_fps_counter
+from edgetpu_server.utils import avg_fps_counter, arg_parser
 from pycoral.utils.dataset import read_label_file
 from pycoral.utils.edgetpu import make_interpreter
 from pycoral.utils.edgetpu import run_inference
@@ -45,29 +45,7 @@ def generate_svg(size, text_lines):
 
 
 def main():
-    default_model_dir = '../all_models'
-    default_model = 'mobilenet_v2_1.0_224_quant_edgetpu.tflite'
-    default_labels = 'imagenet_labels.txt'
-    parser = argparse.ArgumentParser()
-    parser.add_argument('--model', help='.tflite model path',
-                        default=os.path.join(default_model_dir, default_model))
-    parser.add_argument('--labels', help='label file path',
-                        default=os.path.join(default_model_dir, default_labels))
-    parser.add_argument('--top_k', type=int, default=3,
-                        help='number of categories with highest score to display')
-    parser.add_argument('--threshold', type=float, default=0.1,
-                        help='classifier score threshold')
-    parser.add_argument('--videosrc', help='Which video source to use. ',
-                        default='/dev/video0')
-    parser.add_argument('--headless', help='Run without displaying the video.',
-                        default=False, type=bool)
-    parser.add_argument('--videofmt', help='Input video format.',
-                        default='raw',
-                        choices=['raw', 'h264', 'jpeg'])
-    args = parser.parse_args()
-
-    print('Loading {} with {} labels.'.format(args.model, args.labels))
-    run(args)
+    arg_parser("classify", run)
 
 
 def run(args):
--- edgetpu_server/utils/__init__.py
+++ edgetpu_server/utils/__init__.py
@@ -1,5 +1,7 @@
+import argparse
 import collections
 import time
+from typing import Callable
 
 
 def avg_fps_counter(window_size):
@@ -12,3 +14,37 @@ def avg_fps_counter(window_size):
         window.append(curr - prev)
         prev = curr
         yield len(window) / sum(window)
+
+
+def arg_parser(
+        program_name: str,
+        runner: Callable
+):
+    parser = argparse.ArgumentParser(program_name)
+    parser.add_argument('--model',
+                        help='.tflite model path')
+    parser.add_argument('--labels',
+                        help='label file path')
+    parser.add_argument('--top_k',
+                        type=int,
+                        default=3,
+                        help='number of categories with highest score to display')
+    parser.add_argument('--threshold',
+                        type=float,
+                        default=0.1,
+                        help='classifier score threshold')
+    parser.add_argument('--videosrc',
+                        help='Which video source to use.',
+                        default='/dev/video0')
+    parser.add_argument('--headless',
+                        type=bool,
+                        default=False,
+                        help='Run without displaying the video.')
+    parser.add_argument('--videofmt',
+                        default='raw',
+                        choices=['raw', 'h264', 'jpeg'],
+                        help='Input video format.')
+    args = parser.parse_args()
+
+    print('Loading {} with {} labels.'.format(args.model, args.labels))
+    runner(args)
